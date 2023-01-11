df72b809f819641a854d64ea43f7475a169de59a
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2023-01-11 22:57:17 +0100

    first unique string

--- String/LeetCode Easy/First Unique Character in a String.js
+++ String/LeetCode Easy/First Unique Character in a String.js
@@ -1,46 +1,46 @@
-// /**
-//  * @param {string} s
-//  * @return {number}
-//  */
-// const firstUniqChar = function (s) {
-//   const collector = [];
-//   const sArr = [...s];
-//   console.log(sArr);
-//   let finder;
-//   for (let i = 0; i < s.length; i++) {
-//     if (s[i] === s[i + 1]) {
-//       collector.push(s[i]);
-//     }
-//   }
+/**
+ * @param {string} s
+ * @return {number}
+ */
 
-//   for (let j = 0; j < sArr.length; j++) {
-//     finder = sArr.find(element => element === collector[j]);
-//     if (finder === sArr[j]) {
-//       console.log(j);
-//     }
-//   }
+//CHECK "FirstUniqueCharacterImg" IN Img FOLDER FOR THE RUNTIME AND MEMORY
+const firstUniqChar = function(s) {
+  const setArr = {};
 
-//   console.log(finder);
-
-//   console.log(collector);
-// };
-
-// firstUniqChar('loveleetcode');
-// firstUniqChar('aabb');
-
-const longestPrefix = function (strs) {
-  let prefix = '';
-  for (let element of strs) {
-    let next = strs[strs.indexOf(element) + 1];
-    console.log(element);
-    console.log(next);
-    for (let j = 0; j < element.length; j++) {
-      console.log(element[j], next[j]);
-      // console.log(strs[i][j] === next[j]);
-      // if (strs[i][j] === next[j]) prefix += strs[i][j];
+  //CREATING A KEY VALUE OBJECT IN setArr
+  for (let i = 0; i < s.length; i++) {
+    if (!setArr[s[i]]) {
+      setArr[s[i]] = 1;
+    } else {
+      setArr[s[i]]++;
     }
   }
-  // console.log(prefixt);
+
+  //LOOPING THE setArr
+
+  for (let key of Object.keys(setArr)) {
+    if (setArr[key] === 1) return s.indexOf(key);
+  }
+
+  return -1;
 };
 
-longestPrefix(['flower', 'flavour', 'flat']);
+firstUniqChar('loveleetcode');
+firstUniqChar('aabb');
+
+// const longestPrefix = function (strs) {
+//   let prefix = '';
+//   for (let element of strs) {
+//     let next = strs[strs.indexOf(element) + 1];
+//     console.log(element);
+//     console.log(next);
+//     for (let j = 0; j < element.length; j++) {
+//       console.log(element[j], next[j]);
+//       // console.log(strs[i][j] === next[j]);
+//       // if (strs[i][j] === next[j]) prefix += strs[i][j];
+//     }
+//   }
+//   // console.log(prefixt);
+// };
+
+// longestPrefix(['flower', 'flavour', 'flat']);
