5655a6321387bd8b96d84d9d819307254a68aeb2
nueladeyemi1 <emmanuel.adeyemi40@gmail.com>
2024-01-07 23:41:37 +0100

    uncommon words

---
+++ Hash Table/LeetCode Easy/uncommon words from two sentences.py
@@ -0,0 +1,34 @@
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        s1_array = s1.split(' ')
+        s2_array = s2.split(' ')
+        collector = []
+        hash_map = {}
+        
+        
+        
+        for i in range(len(s1_array)):
+            if s1_array[i] not in hash_map:
+                hash_map[s1_array[i]] = 0
+            hash_map[s1_array[i]] += 1
+            
+            
+        for j in range(len(s2_array)):
+            if s2_array[j] not in hash_map:
+                hash_map[s2_array[j]] = 0
+            hash_map[s2_array[j]] += 1
+        
+
+        for val in hash_map:
+            if hash_map[val] == 1:
+                collector.append(val)
+            
+
+            
+        return collector
+
+
+# Runtime: 42 ms, faster than 40.38% of Python3 online submissions for Uncommon Words from Two Sentences.
+# Memory Usage: 17.5 MB, less than 11.30% of Python3 online submissions for Uncommon Words from Two Sentences.
+
+# https://leetcode.com/problems/uncommon-words-from-two-sentences
